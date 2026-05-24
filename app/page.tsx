ca6c7687447f2f245174890501dba1b1ddf88b7e
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2026-05-25 01:06:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import RecentProjects from "@/components/RecentProjects";
 import Articles from "@/components/Articles";
 // import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
-import TestimonialsSection from "@/components/TestimonialsSection";
 import Footer from "@/components/Footer";
 import { navItems } from "@/data";
 import CodingProfiles from "@/components/CodingProfiles";
@@ -24,7 +23,6 @@ export default function Home() {
         <CodingProfiles />
         <Articles />
         <InterviewPlaybooks />
-        <TestimonialsSection />
         <Footer />
       </div>
       <MemeCat />
